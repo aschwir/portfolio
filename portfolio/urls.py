3b879b6117e7cbaedccb2b0e615262f888ae7cfc
Andrew Schwirzinski <schwiraj@mail.uc.edu>
2024-07-05 18:23:14 -0400

    added style.css

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import  path
 from .views import index
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', index, name='home' ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
